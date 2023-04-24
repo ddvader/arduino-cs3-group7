1507774df983321cfae597459685f3e85208f6dc
Edward1409 <edwardpacheco1409@gmail.com>
2023-04-24 14:12:23 -0500

    eddy 2

--- songcode.ino
+++ songcode.ino
@@ -1,12 +1,21 @@
 int output = 13;
-int D5 = 587;
-int F5 = 698;
-int G4 = 392;
-int Bb = 466;
-int C5 = 523;
-int CC5 = 554;
+int G3 = 196; // low G
+int a3 = 228; // low A
+int B3 = 246; // low B
+int C4 = 261; // low C
+int CC4 = 277; // low d flat = low C sharp
+int D4 = 293; // Low d
+int E4 = 329; // E
+int F4 = 349; // f
+int G4 = 392; // middle G
+int a4 = 440; // A
+int Bb = 466; // B flat
+int C5 = 523; // middle C
+int CC5 = 554; // C sharp = D flat
+int D5 = 587; // D
 
-
+int sixtyforth = 32;
+int thirtysecond = 65;
 int sixteenth = 130;
 int eighth = 261;
 int quarter = 552;
@@ -14,6 +23,7 @@ int dotquarter = 828;
 int half = 1104;
 int third = 1656;
 int whole = 2208;
+
 
 
 void setup() {
